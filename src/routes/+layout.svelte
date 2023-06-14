73e386e0cc2dc11ae218e7ca848b8fc077af01f7
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-14 05:40:03 -0700

    formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script lang='ts'>
+<script lang="ts">
 	// Required Skeleton imports
 	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
@@ -7,16 +7,20 @@
 	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-
 <AppShell>
 	<svelte:fragment slot="header">
-
-		<AppBar gridColumns="grid-cols-3" slotDefault="place-content-stretch" slotTrail="place-content-end">
-			<svelte:fragment slot="lead"><span class="font-bold text-xl">🧠 <span class="max-sm:hidden">ArtifEddit</span></span></svelte:fragment>
-			<input class="input  max-sm:hidden" title="search" type="text" placeholder="Search.." />
+		<AppBar
+			gridColumns="grid-cols-3"
+			slotDefault="place-content-stretch"
+			slotTrail="place-content-end"
+		>
+			<svelte:fragment slot="lead"
+				><span class="font-bold text-xl">🧠 <span class="max-sm:hidden">ArtifEddit</span></span
+				></svelte:fragment
+			>
+			<input class="input max-sm:hidden" title="search" type="text" placeholder="Search.." />
 			<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
 		</AppBar>
-
 	</svelte:fragment>
 
 	<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-	import PostPreview from '$lib/PostPreview.svelte'
+	import PostPreview from '$lib/PostPreview.svelte';
 
 	let sample = {
 		title: 'AITA?',
-		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		content:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		author: 'throwaway123'
 	};
-
 </script>
 
 <div class="container h-full mx-auto flex justify-center">
 	<div class="space-y-5 pt-11 items-center">
-		
 		{#each Array(4) as _, i}
 			<PostPreview title={sample.title} content={sample.content} author={sample.author} />
 		{/each}
